ed52393a7a539f9207a644a7ddc424b8b2cfca5f
Chao424 <zhouchao424@outlook.com>
2022-08-19 15:02:40 +0800

    lab1

--- lab01/hello.c
+++ lab01/hello.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
-
-int main(int argc, char *argv[]) {
+int test(int, int);
+int main(int argc, char *argv[])
+{
     int i;
     int count = 0;
     int *p = &count;
-
-    for (i = 0; i < 10; i++) {
+    int k;
+    for (i = 0; i < 10; i++)
+    {
         (*p)++; // Do you understand this line of code and all the other permutations of the operators? ;)
     }
-
+    k = test(i, count);
     printf("Thanks for waddling through this program. Have a nice day.");
     return 0;
+}
+
+int test(int a, int b)
+{
+    int c;
+    c = b + a;
+    c++;
+    return c;
 }
